35bde06532e761226a2a0d0b7ac98d3080647771
Andrii Hodniuk <andrii.hodniuk@outlook.com>
2020-10-28 00:26:15 +0100

    Project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import s from './App.modules.scss';
+
+const App = () => {
+	return (
+		<h1 className={s.header}>
+			This is App Component!
+		</h1>
+	);
+};
+
+export default App;

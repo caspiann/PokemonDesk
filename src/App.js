01fd0e3ca7117b5db5d56909fc1a7f5fd18f5384
Andrii Hodniuk <andrii.hodniuk@outlook.com>
2020-11-03 20:23:28 +0100

    Additional project setup (eslint, prettier, .js => .ts)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-
-import s from './App.modules.scss';
-
-const App = () => {
-	return (
-		<h1 className={s.header}>
-			This is App Component!
-		</h1>
-	);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-
-export default App;
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+var App_module_scss_1 = __importDefault(require("./App.module.scss"));
+var App = function () {
+    return react_1.default.createElement("h1", { className: App_module_scss_1.default.header }, "This is App Component!");
+};
+exports.default = App;
+//# sourceMappingURL=App.js.map
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './App.module.scss';
+
+const App = () => {
+  return <h1 className={s.header}>This is App Component!</h1>;
+};
+
+export default App;

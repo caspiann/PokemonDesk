f19c71eca8fc85a9960bcaee02123a079c7a266f
Andrii Hodniuk <andrii.hodniuk@outlook.com>
2020-11-18 21:03:15 +0100

    HW-6 Separating custom hook from the component; changed the link styles

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,16 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import s from '../RootWrapper.module.scss';
 import Content from '../../components/Content';
 import PokemonCard, { PokemonType } from '../../components/PokemonCard';
 import Heading from '../../components/UI/Heading';
-
-interface TotalData {
-  total: number;
-  count: number;
-  offset: number;
-  limit: string;
-  pokemons: [];
-}
+import usePokemons from '../../hooks/usePokemon';
 
 export interface IPokemon {
   nameClean: string;
@@ -35,35 +28,6 @@ export interface IPokemon {
 }
 
 const Pokedex = () => {
-  const usePokemons = () => {
-    const [data, setData] = useState<TotalData>({ count: 0, limit: '', offset: 0, pokemons: [], total: 0 });
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
-
-    useEffect(() => {
-      const getPokemons = async () => {
-        setIsLoading(true);
-        try {
-          const response = await fetch('http://zar.hosthot.ru/api/v1/pokemons?limit=30');
-          const result = await response.json();
-          setData(result);
-          setIsError(false);
-        } catch (e) {
-          setIsError(true);
-        } finally {
-          setIsLoading(false);
-        }
-      };
-
-      getPokemons();
-    }, []);
-    return {
-      data,
-      isLoading,
-      isError,
-    };
-  };
-
   const { data, isLoading, isError } = usePokemons();
 
   if (isLoading) {
